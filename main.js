b760037801bad1f14609b057900fd4ad2d66efc2
eduardochaves98 <duduchaves98@gmail.com>
2023-03-14 10:30:25 -0300

    Final

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-document.querySelector('.tecla_pom');
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas.forEach( e =>{
+    e.onclick = function (){
+        let idAudio = `#som_${this.classList[1]}`;
+        let element = document.querySelector(idAudio);
+        element == null ? console.log("Not found!!"): element.localName === "audio" ? element.play(): console.log("Not Audio")
+    }
+    e.onkeydown = function (event){
+        if(event.code === 'Enter' || event.code === 'Space'){
+            e.classList.add('ativa');
+        }
+    }
+    e.onkeyup = function (){
+        e.classList.remove('ativa');
+    }
+})
